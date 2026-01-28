6ecb666635036331e4e9f847b1efd52fb4efd8ca
Michael Dodsworth <michael.dodsworth.gnw@gmail.com>
2026-01-28 17:02:45 +0000

    on the bus flag, it should just be the destination, not an approaching s

--- src/components/bus-map.tsx
+++ src/components/bus-map.tsx
@@ -200,7 +200,7 @@ export default function BusMap({
         directionLabel = ` <span style="color:blue">[O]</span>`;
       }
 
-      flagElement.innerHTML = `${bus.fleetNumber} | ${bus.service}${directionLabel} | Approaching: ${bus.nextStop || bus.destination}`;
+      flagElement.innerHTML = `${bus.fleetNumber} | ${bus.service}${directionLabel} | ${bus.destination}`;
 
       // Rotate arrow based on bearing
       const svg = markerElement.querySelector('svg');
